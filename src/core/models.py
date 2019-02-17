236dcb2d89b99683f9a8ce45c10774b497dd918d
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-02-17 21:22:58 +0330

    Create blueprint of core.models

--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    pass
+
+
+class UserType(models.Model):
+    pass
+
+
+class UserStatus(models.Model):
+    pass
+
+
+class Motor(models.Model):
+    pass
+
+
+class MotorType(models.Model):
+    pass
+
+
+class Address(models.Model):
+    pass
+
+
+class Transaction(models.Model):
+    pass
+
+
+class TransactionType(models.Model):
+    pass
+
+
+class TransactionStatus(models.Model):
+    pass
+
+
+class Good(models.Model):
+    pass
+
+
+class RequestGood(models.Model):
+    pass
+
+
+class Request(models.Model):
+    pass
+
+
+class RequestType(models.Model):
+    pass
+
+
+class System(models.Model):
+    pass
