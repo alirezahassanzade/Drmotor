69193b99fd721d332cc5a3b8a67ae5de67a155ac
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-02-26 12:46:13 +0330

    Register all core.models to core.admin

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from core.models import *
 
-# Register your models here.
+admin.site.register(UserStatus)
+admin.site.register(Address)
+admin.site.register(UserType)
+admin.site.register(User)
+admin.site.register(MotorType)
+admin.site.register(Motor)
+admin.site.register(TransactionType)
+admin.site.register(TransactionStatus)
+admin.site.register(Transaction)
+admin.site.register(Good)
+admin.site.register(Request)
+admin.site.register(RequestGood)
+admin.site.register(Service)
+admin.site.register(RequestService)
+admin.site.register(System)
