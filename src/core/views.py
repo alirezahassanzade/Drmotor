35e74634bbdfa9de85610d81137057ff9d4e3d69
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-02-26 15:23:32 +0330

    Add list_goods to core.view's context

--- src/core/views.py
+++ src/core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from .models import Good
+
 
 def home_view(request):
     context = {
@@ -9,7 +11,8 @@ def home_view(request):
 
 
 def shop_view(request):
+    goods_list = Good.objects.all()
     context = {
-
+        'goods_list': goods_list
     }
     return render(request, 'shop.html', context)
