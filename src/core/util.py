7f5ad78228fd4c7b9ebd41501d7c0d48753767ce
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-02-27 15:30:04 +0330

    add get_first_good_pic function

--- src/core/util.py
+++ src/core/util.py
@@ -1,6 +1,6 @@
 from django.db.models import Min, Max
 
-from .models import Good
+from .models import Good, Good_Image, Image
 
 
 def get_min_good_price():
@@ -9,3 +9,12 @@ def get_min_good_price():
 
 def get_max_good_price():
     Good.objects.filter().value_list('Price').annotate(Max('Price')).order_by('price')[0]
+
+
+def get_first_good_pic(good_id):
+    good = Good.objects.filter(id=good_id)[0]
+    good_image = Good_Image.objects.filter(Good=good)
+    if len(good_image) >= 1:
+        return good_image[0].Image.Img.url
+    else:
+        return '/static/img/nopic.png'
