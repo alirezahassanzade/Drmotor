8ae696b6c3a8f15d48d9eae932492a1ae016b62d
Pedram Parsian <Pedram_Parsian@outlook.com>
2019-03-05 17:07:03 +0330

    Complete SignupForm & LoginForm

--- src/core/forms.py
+++ src/core/forms.py
@@ -8,6 +8,6 @@ class LoginForm(forms.Form):
 
 class SignupForm(forms.Form):
     first_name = forms.CharField(widget=forms.TextInput(attrs={'class':'login text5', 'type': 'text2'}), label='')
-    # last_name =
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class':'text5', 'type': 'familyname'}), label='')
     phone_number = forms.CharField(widget=forms.TextInput(attrs={'class':'login text5', 'type': 'text3'}), label='')
     password = forms.CharField(widget=forms.TextInput(attrs={'class':'login text5', 'type': 'text4'}), label='')
