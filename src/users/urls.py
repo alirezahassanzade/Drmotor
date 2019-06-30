e1db830ba5755fcfc9463781741719741914d880
Pedram Parsian <pedram_parsian@outlook.com>
2019-06-30 11:02:46 +0430

    Complete User Model

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import SigninView, SignupView
+from .views import SigninView, SignupView, SignoutView
 
 urlpatterns = [
     # path('profile/', profile_view, name='profile'),
     path('signin/', SigninView.as_view(), name='signin'),
     path('signup/', SignupView.as_view(), name='signup'),
+    path('signout/', SignoutView.as_view(), name='signout'),
 ]
--- src/users/views.py
+++ src/users/views.py
@@ -1,19 +1,10 @@
 from django.shortcuts import render
 import logging
-from datetime import timedelta
-
 from django.conf import settings
-from django.utils import timezone
-from django.utils.decorators import method_decorator
-
 from django.contrib import messages
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.views import LoginView, LogoutView
-
-from django.views.generic import View
 from django.views.generic.edit import FormView
-from django.views.decorators.cache import never_cache
-
 from .forms import UserCreationForm, AuthenticationForm
 
 
@@ -34,7 +25,6 @@ class SignupView(FormView):
     def form_valid(self, form):
         response = super().form_valid(form)
         form.save()
-
 
         phone_number = form.cleaned_data.get("phone_number")
         raw_password = form.cleaned_data.get("password1")
@@ -61,14 +51,6 @@ def profile_view(request):
 
 class SignoutView(LogoutView):
     template_name = "users/signout.html"
-    extra_context = {}
-
-    @method_decorator(never_cache)
-    def dispatch(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            self.extra_context['was_authenticated'] = True
-            self.extra_context['first_name'] = request.user.first_name
-        return super().dispatch(request, *args, **kwargs)
 
     def get_next_url(self):
         redirect_to = self.request.GET.get("next", "/")
