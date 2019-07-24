cd128f992b7f11d82abb74cc867897c9046ef6f3
Pedram Parsian <pedram_parsian@outlook.com>
2019-07-24 12:26:20 +0430

    fix bug

--- src/shop/admin.py
+++ src/shop/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 
-from .models import Product, Image, Color, Category, Tag, Basket, BasketLine, Commentor, Comment
+from .models import Product, Image, Color, Category, Tag, Basket, BasketLine, Comment
 
 
 admin.site.register(Product)
-admin.site.register(Commentor)
 admin.site.register(Comment)
 admin.site.register(Image)
 admin.site.register(Color)
